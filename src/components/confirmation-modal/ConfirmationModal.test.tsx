fc2a730652723baa3c2a429af6f3e62d8fc7283e
Alyx DARENNE <axda.web@gmail.com>
2023-07-04 10:19:45 +0200

    Fix eslint error

--- src/components/confirmation-modal/ConfirmationModal.test.tsx
+++ src/components/confirmation-modal/ConfirmationModal.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 import { MemoryRouter } from "react-router-dom";
 
 import ConfirmationModal from "./ConfirmationModal";
